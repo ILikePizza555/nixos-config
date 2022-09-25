53a4dcece00770701606b5648aa767ee397ca5d5
Izzy Lancaster <=>
2022-09-24 19:39:47 -0700

    Added nev-systems and nixos-generators

--- flake.nix
+++ flake.nix
@@ -5,24 +5,57 @@
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+     nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager }: {
-    nixosConfigurations."vm-goth-pinkie-pie" = nixpkgs.lib.nixosSystem {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-generators }:
+  let
+    nev-systems-modules = [
+      ./hosts/nev-systems
+      home-manager.nixosModules.home-manager
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          users.izzylan = import ./users/izzylan.nix;
+        };
+      }
+    ];
+  in
+  {
+    packages.x86_64-linux."nev-systems-install-iso" = nixos-generators.nixosGenerate {
       system = "x86_64-linux";
       specialArgs = inputs;
-      modules = [
-        ./hosts/goth-pinkie-pie
-        ./profiles/neovim.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPackages = true;
-            useUserPackages = true;
-            users.izzylan = import ./users/izzylan.nix
-          };
-        }
-      ];
+      modules = nev-systems-modules;
+      format = "install-iso";
+    };
+
+    nixosConfigurations = {
+      vm-goth-pinkie-pie = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = [
+          ./hosts/goth-pinkie-pie
+          ./profiles/neovim.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.izzylan = import ./users/izzylan.nix;
+            };
+          }
+        ];
+      };
+
+      nev-systems = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = nev-systems-modules;
+      };
     };
   };
 }
