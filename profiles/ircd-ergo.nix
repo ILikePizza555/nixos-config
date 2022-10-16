593bb557ce5d46927b60144f9702f4b51cd7ef29
Izzy Lancaster <=>
2022-10-15 22:13:46 -0700

    Fix ircd-ergo configuration

--- profiles/ircd-ergo.nix
+++ profiles/ircd-ergo.nix
@@ -16,57 +16,55 @@ in
   services = {
     ergochat-redux = {
       enable = true;
-      settings = {
-        accounts = {
-          authentication-enabled = true;
-          registration.enabled = false;
-          require-sasl.enabled = true;
-        };
-        datastore.mysql = {
-          enabled = true;
-          ensureDB = true;
-        };
-        history = {
-          enabled = true;
-          persistent = {
-            enabled = true;
-            direct-messages = "opt-in";
-          };
-          retention = {
-            allow-individual-delete = true;
-            enable-account-indexing = true;
-          };
-        };
-        server = {
-          name = "irc.nev.systems";
-          listeners = {
-            # Localhost listeners for administration
-            "127.0.0.1:6667" = {};
-            "[::1]:6667" = {};
-
-            ":6697" = {
-              inherit useACMEHost;
-            };
-
-            # Websocket listner
-            ":8097" = {
-              websocket = true;
-              inherit useACMEHost;
-            };
-          };
-          sts.enabled = true;
-          websockets.allowed-origins = [ host ];
-          enforce-utf8 = true;
-        };
-        roleplay.enabled = true;
+      accounts = {
+        authentication-enabled = true;
+        registration.enabled = false;
+        require-sasl.enabled = true;
       };
+      datastore.mysql = {
+        enabled = true;
+        ensureDB = true;
+      };
+      history = {
+        enabled = true;
+        persistent = {
+          enabled = true;
+          direct-messages = "opt-in";
+        };
+        retention = {
+          allow-individual-delete = true;
+          enable-account-indexing = true;
+        };
+      };
+      server = {
+        name = "irc.nev.systems";
+        listeners = {
+          # Localhost listeners for administration
+          "127.0.0.1:6667" = {};
+          "[::1]:6667" = {};
+
+          ":6697" = {
+            inherit useACMEHost;
+          };
+
+          # Websocket listner
+          ":8097" = {
+            websocket = true;
+            inherit useACMEHost;
+          };
+        };
+        sts.enabled = true;
+        websockets.allowed-origins = [ host ];
+        enforce-utf8 = true;
+      };
+      roleplay.enabled = true;
     };
+  };
 
-    nginx.virtualHosts.${host} = {
-      inherit useACMEHost;
-      locations."/" = {
-        return = "204";
-      };
+  nginx.virtualHosts.${host} = {
+    inherit useACMEHost;
+    locations."/" = {
+      return = "204";
     };
   };
 }
