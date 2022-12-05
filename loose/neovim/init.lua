b4c6bdf33487032bb6e628f8a498f6286c090628
Izzy Lancaster <=>
2022-12-04 17:05:02 -0800

    Added missing arguments in init.lua

--- loose/neovim/init.lua
+++ loose/neovim/init.lua
@@ -12,6 +12,6 @@ vim.o.relativenumber = true
 -- Keybinds (https://neovim.io/doc/user/api.html#nvim_set_keymap())
 -- Telescope keybinds
 vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files<cr>', {})
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>');
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tdags<cr>');
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {});
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tdags<cr>', {});
