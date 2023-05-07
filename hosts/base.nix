d2ad87399a39a75a902c4aa5ddf5cd7499a970e0
Izzy Lancaster <=>
2023-05-06 20:22:30 -0700

    Retabed base.nix

--- hosts/base.nix
+++ hosts/base.nix
@@ -2,39 +2,39 @@
 { config, pkgs, ...}:
 
 {
-  environment = {
-    systemPackages = [
-      pkgs.git
-      pkgs.ripgrep
-      pkgs.tree
-      # For encrypting secrets
-      pkgs.age
-      # Syntax highlighted `cat`
-      pkgs.bat 
-      pkgs.wget
-    ];
-  };
+	environment = {
+		systemPackages = [
+			pkgs.git
+			pkgs.ripgrep
+			pkgs.tree
+			# For encrypting secrets
+			pkgs.age
+			# Syntax highlighted `cat`
+			pkgs.bat 
+			pkgs.wget
+		];
+	};
 
-  # We have to enable nixFlakes since this is a flake
-  nix = {
+	# We have to enable nixFlakes since this is a flake
+	nix = {
 		extraOptions = ''experimental-features = nix-command flakes'';
-  };
+	};
 
-  programs = {
-    fish = {
-      enable = true;
-    };
+	programs = {
+		fish = {
+			enable = true;
+		};
 
-    tmux = {
-      enable = true;
-    };
-  };
+		tmux = {
+			enable = true;
+		};
+	};
 
 	time = {
 		timeZone = "America/Los_Angeles";
 	};
 
-  users = {
-    defaultUserShell = pkgs.fish;
-  };
+	users = {
+		defaultUserShell = pkgs.fish;
+	};
 }
