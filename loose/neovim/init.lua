54f26287d94f5b7aab4f858414f5baab9dda8450
Izzy Lancaster <=>
2023-05-06 18:55:32 -0700

    Neovim config changes

--- loose/neovim/init.lua
+++ loose/neovim/init.lua
@@ -4,20 +4,37 @@ local trouble = require'trouble'
 local cmp = require'cmp'
 local neogit = require'neogit'
 
-vim.cmd.colorscheme('nightfox')
 vim.cmd.syntax('on')
 
 vim.o.autoindent = true
 vim.o.smartindent = true
-vim.o.expandtab = false
+vim.o.noexpandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
 vim.o.relativenumber = true
 
+-- Not quite part of indent-blankline, but closely related
+vim.o.list = true
+vim.opt.listchars:append "lead:⋅"
+vim.opt.listchars:append "eol:↴"
+
 neogit.setup({})
 
 require('lualine').setup()
+
+-- indent-blankline setp
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+require("indent_blankline").setup({
+	show_end_of_line = true
+})
+
+-- Onedark config
+require('onedark').setup({
+	style = 'warmer',
+	toggle_style_key = '<Leader>ts'
+})
+vim.cmd.colorscheme('onedark')
 
 -- Keybinds (https://neovim.io/doc/user/api.html#nvim_set_keymap())
 -- Telescope keybinds
@@ -34,26 +51,26 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist,	{ noremap = true, sil
 
 local rust_lsp_on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD',					vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd',					vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K',					vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi',					vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>',			vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa',	vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr',	vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D',		vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn',	vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca',	vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr',					vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	vim.keymap.set('n', 'gD',					vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd',					vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K',					vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi',					vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>',			vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa',	vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr',	vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D',		vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn',	vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca',	vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr',					vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- LSP Configuration
@@ -91,13 +108,13 @@ cmp.setup({
 
 -- Discord rich presence
 require("presence"):setup({
-	auto_update         = true,
+	auto_update					= true,
 	log_level						= "info",
-	editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-  file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-  git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-  plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-  reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-  workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-  line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+	editing_text				= "Editing %s",								-- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+	file_explorer_text	= "Browsing %s",							-- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+	git_commit_text			= "Committing changes",				-- Format string rendered when committing changes in git (either string or function(filename: string): string)
+	plugin_manager_text	= "Managing plugins",					-- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+	reading_text				= "Reading %s",								-- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+	workspace_text			= "Working on %s",						-- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+	line_number_text		= "Line %s out of %s",				-- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
 })
--- profiles/neovim.nix
+++ profiles/neovim.nix
@@ -1,7 +1,7 @@
 /*
 * This module contains configuration for a system-level neovim.
 * Todo: Refactor this into a system module. Some points to consider:
-*   - We can reuse `wrapNeovimUnstable`, so long as we pass `wrapRc = false` to prevent it from create an init.vim
+*		- We can reuse `wrapNeovimUnstable`, so long as we pass `wrapRc = false` to prevent it from create an init.vim
 *		- `makeNeovimConfig` lets you specify per-plugin configuration (by passing in an attrset with the `config` option set), which would get built into the init.vim file. Perhaps the module should have a similar functionality for lua?
 		- Still need to figure out what `extraPythonPackages` and `extraLuaPackages` do in https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/neovim/utils.nix
 	Important sources (which I used to help me construct this file):
@@ -13,48 +13,49 @@
 {pkgs, lib, ...}:
 
 let
-  neovimInitFile = ./../loose/neovim/init.lua;
+	neovimInitFile = ./../loose/neovim/init.lua;
 
-  neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
-    withPython3 = true;
-    withNodeJs = true;
+	neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+		withPython3 = true;
+		withNodeJs = true;
 		# plugins can be a listof attrset with {plugin: package, config: string, options: bool} or just a list of packages.
-    plugins = with pkgs.vimPlugins; [
-      nightfox-nvim
-      vim-nix
-      indent-blankline-nvim
-      plenary-nvim
-      telescope-nvim
-      neogit
-      lualine-nvim
-      nvim-web-devicons
-      barbar-nvim
-      nvim-lspconfig
-      rust-tools-nvim
-      trouble-nvim
-      luasnip
-      nvim-cmp
-      cmp-nvim-lsp
-      cmp_luasnip
-      cmp-buffer
-      presence-nvim
-    ];
-  };
+		plugins = with pkgs.vimPlugins; [
+			nightfox-nvim
+			onedark-nvim
+			vim-nix
+			indent-blankline-nvim
+			plenary-nvim
+			telescope-nvim
+			neogit
+			lualine-nvim
+			nvim-web-devicons
+			barbar-nvim
+			nvim-lspconfig
+			rust-tools-nvim
+			trouble-nvim
+			luasnip
+			nvim-cmp
+			cmp-nvim-lsp
+			cmp_luasnip
+			cmp-buffer
+			presence-nvim
+		];
+	};
 
-  neovimPackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (neovimConfig // {
-    wrapperArgs = neovimConfig.wrapperArgs ++ [ "--add-flags" "-u ${neovimInitFile}"];
-    # Disable the auto generation of init.vim, because we're using a lua init
-    wrapRc = false;
-  }); 
+	neovimPackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (neovimConfig // {
+		wrapperArgs = neovimConfig.wrapperArgs ++ [ "--add-flags" "-u ${neovimInitFile}"];
+		# Disable the auto generation of init.vim, because we're using a lua init
+		wrapRc = false;
+	}); 
 in {
-  environment = {
-    systemPackages = [
-        # Required for telescope
-        pkgs.ripgrep
-        pkgs.fd
+	environment = {
+		systemPackages = [
+				# Required for telescope
+				pkgs.ripgrep
+				pkgs.fd
 
-        neovimPackage
-      ];
-    variables.EDITOR = lib.mkOverride 900 "nvim";
-  };
+				neovimPackage
+			];
+		variables.EDITOR = lib.mkOverride 900 "nvim";
+	};
 }
