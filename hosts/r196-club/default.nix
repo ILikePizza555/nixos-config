b262ae01d5fbf22714ada3dda06d4a133bd7841d
Izzy Lancaster <avrisaac555@gmail.com>
2023-06-12 00:09:24 -0700

    Retab default.nix and use systemd boot

--- hosts/r196-club/default.nix
+++ hosts/r196-club/default.nix
@@ -1,53 +1,53 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ../base.nix
-    ./hardware-configuration.nix
-  ];
+	imports = [
+		../base.nix
+		./hardware-configuration.nix
+	];
 
-  age = {
-    secrets = {
-      namecheapApi.file = ../../secrets/namecheapapi.age;
-    };
-  };
+	age = {
+		secrets = {
+			namecheapApi.file = ../../secrets/namecheapapi.age;
+		};
+	};
 
-  boot = {
-    loader.grub.device = "/dev/vda";
-  };
+	boot = {
+		loader.systemd-boot.enable = true;
+	};
 
-  networking = {
-    hostName = "r196-club";
-    firewall = {
-      # 22 - Opened for ssh
-      # 80 443 - Opened for web
-      allowedTCPPorts = [22 80 443];
-    };
-  };
+	networking = {
+		hostName = "r196-club";
+		firewall = {
+			# 22 - Opened for ssh
+			# 80 443 - Opened for web
+			allowedTCPPorts = [22 80 443];
+		};
+	};
 
-  services = {
-    openssh = {
-      enable = true;
-      passwordAuthentication = false;
-      permitRootLogin = "no";
-    };
-  };
+	services = {
+		openssh = {
+			enable = true;
+			passwordAuthentication = false;
+			permitRootLogin = "no";
+		};
+	};
 
-  users = {
-    mutableUsers = false;
+	users = {
+		mutableUsers = false;
 
-    users.izzylan = {
-      isNormalUser = true;
-      description = "Izzy Lancaster personal account";
-      extraGroups = ["wheel"];
-      hashedPassword = "$6$2jTbqagpP2iqzvFR$5Rm/2tc0atsHbtt6iHM5D0BZmbBD612CvlgrlC/ZjUwr3u0MNG/KJxc9ai67qvhxlTX7CM5vf7SU7It.mVbcv0";
-      openssh.authorizedKeys.keyFiles = [
-        ../../keys/izzylan/pinkie-pie
-        ../../keys/izzylan/ipad
-        ../../keys/izzylan/izzy-luna-se
-      ];
-    };
-  };
+		users.izzylan = {
+			isNormalUser = true;
+			description = "Izzy Lancaster personal account";
+			extraGroups = ["wheel"];
+			hashedPassword = "$6$2jTbqagpP2iqzvFR$5Rm/2tc0atsHbtt6iHM5D0BZmbBD612CvlgrlC/ZjUwr3u0MNG/KJxc9ai67qvhxlTX7CM5vf7SU7It.mVbcv0";
+			openssh.authorizedKeys.keyFiles = [
+				../../keys/izzylan/pinkie-pie
+				../../keys/izzylan/ipad
+				../../keys/izzylan/izzy-luna-se
+			];
+		};
+	};
 
-  system.stateVersion = "23.05";
+	system.stateVersion = "23.05";
 }
