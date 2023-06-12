e2e23563c283ec922cd48976ca9ce45d3825c2e8
Izzy Lancaster <avrisaac555@gmail.com>
2023-06-11 20:22:24 -0700

    Retabed izzylan.nix

--- users/izzylan.nix
+++ users/izzylan.nix
@@ -1,30 +1,30 @@
 {config, lib, pkgs, ...}:
 {
-  imports = [
-    ../modules/auto-fix-vscode-server.nix
-  ];
+    imports = [
+        ../modules/auto-fix-vscode-server.nix
+    ];
 
-  home = {
-    username = "izzylan";
-    homeDirectory = "/home/izzylan";
-    stateVersion = "22.05";
-  };
-
-  programs = {
-    direnv = {
-      enable = true;
-      enableFishIntegration = true;
-      nix-direnv.enable = true;
+    home = {
+        username = "izzylan";
+        homeDirectory = "/home/izzylan";
+        stateVersion = "22.05";
     };
 
-    git = {
-      enable = true;
-      userName = "izzylan";
-      userEmail = "avrisaac555@gmail.com";
-    };
+    programs = {
+        direnv = {
+            enable = true;
+            enableFishIntegration = true;
+            nix-direnv.enable = true;
+        };
 
-    home-manager = {
-      enable = true;
+        git = {
+            enable = true;
+            userName = "izzylan";
+            userEmail = "avrisaac555@gmail.com";
+        };
+
+        home-manager = {
+            enable = true;
+        };
     };
-  };
 }
