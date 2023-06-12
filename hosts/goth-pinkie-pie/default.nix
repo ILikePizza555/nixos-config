a35a54a19f833793d45666dbfc5b8a995d86f601
Izzy Lancaster <avrisaac555@gmail.com>
2023-06-11 20:15:26 -0700

    Fix tabs

--- hosts/goth-pinkie-pie/default.nix
+++ hosts/goth-pinkie-pie/default.nix
@@ -1,68 +1,68 @@
 { config, pkgs, nixos-generators, ... }:
 
 {
-	imports = [
-    ../base.nix
-		./hardware-configuration.nix
-	];
-
-	# Use the systemd-boot EFI boot loader.
-	boot = {
-		loader = {
-			systemd-boot.enable = true;
-			efi.canTouchEfiVariables = true;
-		};
-	};
-
-  environment = {
-    systemPackages = [
-      nixos-generators.defaultPackage.${pkgs.system}
+    imports = [
+        ../base.nix
+        ./hardware-configuration.nix
     ];
-  };
 
-	networking = {
-		hostName = "vm-goth-pinkie-pie";
-		firewall = {
-      allowedTCPPorts = [22];
-      allowedTCPPortRanges = [
-        { from = 1000; to = 1500; }
-      ];
-		};
-	};
+    # Use the systemd-boot EFI boot loader.
+    boot = {
+        loader = {
+            systemd-boot.enable = true;
+            efi.canTouchEfiVariables = true;
+        };
+    };
 
-  programs = {
-    mosh.enable = true;
-  };
+    environment = {
+        systemPackages = [
+            nixos-generators.defaultPackage.${pkgs.system}
+        ];
+    };
 
-	services = {
-    tailscale.enable = true;
-		openssh.enable = true;
-	};
+    networking = {
+        hostName = "vm-goth-pinkie-pie";
+        firewall = {
+            allowedTCPPorts = [22];
+            allowedTCPPortRanges = [
+                { from = 1000; to = 1500; }
+            ];
+        };
+    };
 
-	users = {
-		users.izzylan = {
-			isNormalUser = true;
-			description = "Izzy Lancaster account";
-			extraGroups = ["wheel"];
-			openssh.authorizedKeys.keyFiles = [
-				../../keys/izzylan/pinkie-pie
-				../../keys/izzylan/ipad
-				../../keys/izzylan/izzy-luna-se
-			];
-		};
-	};
+    programs = {
+        mosh.enable = true;
+    };
 
-  virtualisation = {
-    hypervGuest.enable = true;
-  };
+    services = {
+        tailscale.enable = true;
+            openssh.enable = true;
+    };
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "22.05"; # Did you read the comment?
+    users = {
+        users.izzylan = {
+            isNormalUser = true;
+            description = "Izzy Lancaster account";
+            extraGroups = ["wheel"];
+            openssh.authorizedKeys.keyFiles = [
+                ../../keys/izzylan/pinkie-pie
+                ../../keys/izzylan/ipad
+                ../../keys/izzylan/izzy-luna-se
+            ];
+        };
+    };
+
+    virtualisation = {
+        hypervGuest.enable = true;
+    };
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "22.05"; # Did you read the comment?
 
 }
 
